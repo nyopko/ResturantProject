976ec71f78d7653af3bc82a283c954e83b12a2be
davidferro <david-ferro@sbcglobal.net>
2019-01-29 20:12:19 -0500

    will this server work?  who knows?

--- server.js
+++ server.js
@@ -10,25 +10,30 @@ var PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var diners = [
-    {
+var tables = [];
 
-    }
-]
-
+var waitlist = [];
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
+    res.sendFile(path.join(__dirname, "home.html"));
   });
   
-  app.get("/add", function(req, res) {
-    res.sendFile(path.join(__dirname, "add.html"));
+  app.get("/reservations", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
+
+  app.get("/tables", function(req, res) {
+      res.sendFile(path.join(__dirname, "tables.html"));
   });
   
   // Displays all tables
   app.get("/api/tables", function(req, res) {
-    return res.json(diners);
+    return res.json(tables);
   });
+
+app.get("/api/waitlist", function(req, res) {
+    return res.json(waitlist);
+});
 
   // Displays a single character, or returns false
 app.get("/api/tables/:diner", function(req, res) {
@@ -36,28 +41,44 @@ app.get("/api/tables/:diner", function(req, res) {
   
     console.log(chosen);
   
-    for (var i = 0; i < diners.length; i++) {
-      if (chosen === diners[i].routeName) {
-        return res.json(diners[i]);
+    for (var i = 0; i < tables.length; i++) {
+      if (chosen === tables[i].customerID) {
+        return res.json(tables[i]);
       }
     }
   
     return res.json(false);
   });
+
+  app.get("/api/waitlist/:diner", function(req, res) {
+      var chosen = req.params.diner;
+      console.log(chosen);
+
+      for (var i = 0; i < waitlist.length; i++) {
+          if (chosen === waitlist[i].customerID) {
+              return res.json(waitlist[i]);
+          }
+      }
+
+      return res.json(false);
+  });
   
   // Create New Characters - takes in JSON input
-  app.post("/api/tables", function(req, res) {
+  app.post("/reservations", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     var newDiner = req.body;
   
     // Using a RegEx Pattern to remove spaces from newCharacter
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newDiner.routeName = newDiner.name.replace(/\s+/g, "").toLowerCase();
+    newDiner.customerID = newDiner.name.replace(/\s+/g, "").toLowerCase();
   
     console.log(newDiner);
-  
-    diners.push(newDiner);
+    if (tables.length < 5) {
+        tables.push(newDiner);
+    } else {
+        waitlist.push(newDiner);
+    }
   
     res.json(newDiner);
   });
